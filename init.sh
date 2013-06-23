554708e436f0476aab6ede939fa91fa4e55fe07f
Hilfi M. Alkaff <alkaff2@illinois.edu>
2013-06-23 10:46:43 -0600

    Modify init.

--- init.sh
+++ init.sh
@@ -3,11 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-6-jre openjdk-6-jdk
 
-sudo chown halkaff /mnt/extra -R
-tar -xzpvf hadoop-1.1.2.tar.gz -C /mnt/extra
-cp conf /mnt/extra/hadoop-1.1.2/ -r
-sudo chown halkaff /mnt/extra -R
+cp conf .. -r
 sudo cp hosts /etc/hosts
 
-cd ../dotfiles/
+cd ~/dotfiles/
 ./setup.sh
