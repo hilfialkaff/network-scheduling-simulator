94addc1800335957f867c224ffc8392c4b9ae5a9
Hilfi M. Alkaff <alkaff2@illinois.edu>
2013-06-26 11:13:58 -0600

    Update.

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ LOG_DIR=$SRC_DIR/logs
 # DFS IO benchmark
 function run_dfsio_bm {
     echo "Running dfsio benchmark..."
-    for i in {1..2..1}
+    for i in {20..100..20}
     do
-        for j in {10..20..10}
+        for j in {1..5..1}
         do
             echo -e "\n"$i $j >> $LOG_DIR/dfs_io.log
-            (time hadoop jar hadoop-*test*.jar TestDFSIO -write -nrFiles $i -fileSize $j &> $LOG_DIR/hadoop.log) >> $LOG_DIR/dfs_io.log 2>&1
-            (time hadoop jar hadoop-*test*.jar TestDFSIO -read -nrFiles $i -fileSize $j &> $LOG_DIR/hadoop.log) >> $LOG_DIR/dfs_io.log 2>&1
+            (time hadoop jar hadoop-*test*.jar TestDFSIO -write -nrFiles $i -fileSize 100 &> $LOG_DIR/hadoop.log) >> $LOG_DIR/dfs_io.log 2>&1
+            (time hadoop jar hadoop-*test*.jar TestDFSIO -read -nrFiles $i -fileSize 100 &> $LOG_DIR/hadoop.log) >> $LOG_DIR/dfs_io.log 2>&1
             hadoop jar hadoop-*test*.jar TestDFSIO -clean &> /dev/null
         done
     done
@@ -24,10 +24,10 @@ function run_dfsio_bm {
 # Terasort benchmark
 function run_terasort_bm {
     echo "Running terasort benchmark..."
-    for i in {100000..200000..100000}
+    for i in {20000000000..10000000000..2000000000}
     do
         hadoop jar hadoop-*examples*.jar teragen $i /data/input &> /dev/null
-        for j in {1..2..1}
+        for j in {1..5..1}
         do
             echo -e "\n"$i $j >> $LOG_DIR/terasort.log
             (time hadoop jar hadoop-*examples*.jar terasort /data/input /data/output &> $LOG_DIR/hadoop.log) >> $LOG_DIR/terasort.log 2>&1
@@ -39,7 +39,7 @@ function run_terasort_bm {
 
 function run_mrbench_bm {
     echo "Running mrbench benchmark..."
-    (time hadoop jar hadoop-*test*.jar mrbench -numRuns 3) &>> $LOG_DIR/mrbench.log
+    (time hadoop jar hadoop-*test*.jar mrbench -numRuns 50) &>> $LOG_DIR/mrbench.log
 }
 
 # Clean up
@@ -49,4 +49,4 @@ hadoop dfs -rmr /data/
 
 run_dfsio_bm
 run_terasort_bm
-# run_mrbench_bm
+run_mrbench_bm
