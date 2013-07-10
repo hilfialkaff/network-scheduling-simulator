2848fc150e87d4e01fe50ddea2314f19c95c5d62
Hilfi M. Alkaff <alkaff2@illinois.edu>
2013-07-10 14:58:39 -0500

    Tweak.

--- _init.sh
+++ _init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "10.1.31.3\tmaster" | sudo tee -a /etc/hosts
+echo -e "10.0.0.2\tmaster" | sudo tee -a /etc/hosts
 sudo apt-get update
 sudo apt-get install -y openjdk-6-jre openjdk-6-jdk maven build-essential protobuf-compiler autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev
 
