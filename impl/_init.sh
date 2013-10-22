334f93c4aa8dd76ca337ccf5c7abc75b208c387a
Hilfi M. Alkaff <alkaff2@illinois.edu>
2013-10-21 19:58:15 -0500

    Removed Hadoop.

--- impl/_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "10.0.0.2\tmaster" | sudo tee -a /etc/hosts
-sudo apt-get update
-sudo apt-get install -y openjdk-6-jre openjdk-6-jdk maven build-essential protobuf-compiler autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev
-
-sudo ./init.sh
--- impl/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export HADOOP_HOME="/proj/uiucnet/exp/tomography/hadoop-1.1.2"
-export HDFS_PATH="/proj/uiucnet/exp/tomography/hdfs/"
-export PATH="$HADOOP_HOME/bin/:$PATH"
-export CLASSPATH="/proj/uiucnet/exp/tomography/hadoop-1.1.2/lib:$CLASSPATH"
